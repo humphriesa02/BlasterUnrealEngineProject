26d016e0cf4a2458c0db5ea70a42ce7226f245a9
Hero <humphriesa02@gmail.com>
2024-01-31 14:37:57 -0800

    Up to 202.

--- Source/Blaster/Weapon/Projectile.h
+++ Source/Blaster/Weapon/Projectile.h
@@ -17,6 +17,17 @@ public:
 
 	virtual void Destroyed() override;
 
+	/*
+	* Used with server-side rewind
+	*/
+
+	bool bUseServerSideRewind = false;
+	FVector_NetQuantize TraceStart;
+	FVector_NetQuantize100 InitialVelocity;
+
+	UPROPERTY(EditAnywhere)
+	float InitialSpeed = 15000.f;
+
 protected:
 	virtual void BeginPlay() override;
 	void StartDestroyTimer();
